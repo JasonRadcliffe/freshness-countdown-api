4ae0ce59442b55fa602587f1be16f5a0be0960db
Jason Radcliffe <jasonjradcliffe@gmail.com>
2020-04-23 17:14:17 +0200

    switching all routes to use new handler instead of controllers

--- app/routes.go
+++ app/routes.go
@@ -1,37 +1,30 @@
 package app
 
-import (
-	"github.com/jasonradcliffe/freshness-countdown-api/controllers/dish"
-	"github.com/jasonradcliffe/freshness-countdown-api/controllers/ping"
-	"github.com/jasonradcliffe/freshness-countdown-api/controllers/storage"
-	"github.com/jasonradcliffe/freshness-countdown-api/controllers/user"
-)
-
 func mapRoutes() {
-	router.GET("/ping", ping.Ping)
+	router.GET("/ping", apiHandler.Ping)
 
-	router.GET("/dishes", dish.GetDishes)
+	router.GET("/dishes", apiHandler.GetDishes)
 
 	//Handles the routes for "/dish/7" and "/dish/expired"
 	router.GET("/dishes/:dish_id", apiHandler.GetDishHandler)
 
-	router.POST("/dishes", dish.CreateDish)
-	router.PATCH("/dishes/:dish_id", dish.UpdateDish)
-	router.DELETE("/dishes/:dish_id", dish.DeleteDish)
+	router.POST("/dishes", apiHandler.CreateDish)
+	router.PATCH("/dishes/:dish_id", apiHandler.UpdateDish)
+	router.DELETE("/dishes/:dish_id", apiHandler.DeleteDish)
 
-	router.GET("/users", user.GetUsers)
-	router.GET("/users/:user_id", user.GetUser)
-	router.POST("/users", user.CreateUser)
-	router.PATCH("/users/:user_id", user.UpdateUser)
-	router.DELETE("/users/:user_id", user.DeleteUser)
+	router.GET("/users", apiHandler.GetUsers)
+	router.GET("/users/:user_id", apiHandler.GetUserHandler)
+	router.POST("/users", apiHandler.CreateUser)
+	router.PATCH("/users/:user_id", apiHandler.UpdateUser)
+	router.DELETE("/users/:user_id", apiHandler.DeleteUser)
 
-	router.GET("/storage", storage.GetStorageUnits)
-	router.GET("/storage/:storage_id", storage.GetStorageUnit)
-	router.POST("/storage", storage.CreateStorageUnit)
-	router.PATCH("/storage/:storage_id", storage.UpdateStorageUnit)
-	router.DELETE("/storage/:storage_id", storage.DeleteStorageUnit)
+	router.GET("/storage", apiHandler.GetStorageUnits)
+	router.GET("/storage/:storage_id", apiHandler.GetStorageByID)
+	router.POST("/storage", apiHandler.CreateStorageUnit)
+	router.PATCH("/storage/:storage_id", apiHandler.UpdateStorageUnit)
+	router.DELETE("/storage/:storage_id", apiHandler.DeleteStorageUnit)
 
-	router.GET("/storage/:storage_id/dish", dish.GetStorageDishes)
+	router.GET("/storage/:storage_id/dish", apiHandler.GetStorageDishes)
 
 	router.GET("/login", Login)
 	router.GET("/oauthlogin", Oauthlogin)
