4842c67e9e8e1f114ecb3c717c96b6dfa933fe02
Jason Radcliffe <jasonjradcliffe@gmail.com>
2020-04-28 16:35:38 +0200

    initial test file added

---
+++ services/user/user-service_test.go
@@ -0,0 +1,16 @@
+package user
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetByEmail(t *testing.T) {
+	assert.Equal(t, "", "")
+
+	test := 2
+	fmt.Println("test", test)
+
+}
