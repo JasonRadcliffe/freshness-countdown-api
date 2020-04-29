2fcf57018c3d548e78f323edb9078a7a6bd887ef
Jason Radcliffe <jasonjradcliffe@gmail.com>
2020-04-29 22:06:45 +0200

    adds the framework for the user service tests

--- services/user/user-service_test.go
+++ services/user/user-service_test.go
@@ -9,8 +9,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestUser_GetByID(t *testing.T) {
+	assert.Equal(t, "", "")
+}
+
+func TestUser_GetByEmail(t *testing.T) {
+	assert.Equal(t, "", "")
+}
+
+func TestUser_Create(t *testing.T) {
+	assert.Equal(t, "", "")
+}
+
+func TestUser_GenerateTempMatch(t *testing.T) {
+	assert.Equal(t, "", "")
+}
+
 func TestGetByEmail(t *testing.T) {
-	
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
 	assert.Equal(t, "", "")
 
 	test := 2
