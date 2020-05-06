f7e57e7fbccfe679b89d3f6facdcf41256df494f
Jason Radcliffe <jasonjradcliffe@gmail.com>
2020-05-06 17:26:16 +0200

    15 bytes for the tempMatch

--- repository/db/db_repository.go
+++ repository/db/db_repository.go
@@ -859,7 +859,7 @@ func (repo *repository) GetStorageDishes(sID int) (*dish.Dishes, fcerr.FCErr) {
 }
 
 func generateTempMatch() string {
-	n := make([]byte, 8)
+	n := make([]byte, 15)
 	rand.Read(n)
 	fmt.Println("New way:", base64.URLEncoding.EncodeToString(n))
 
